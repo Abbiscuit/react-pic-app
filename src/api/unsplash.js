8eb390d7a24cd723191299c015532873710ad9eb
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-05-30 21:16:42 +0900

    Updated axios

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID "
+    Authorization:
+      "Client-ID 52272278c5839d97aa78431e1cbda11ea9afd3e8562f06f09b1aba118a480a38"
   }
 });
